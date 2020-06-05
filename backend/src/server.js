f193b3462d1fa584dafbcc31589cf84ab578905a
David <davidfilipeamorim@gmail.com>
2020-06-05 15:42:19 +0100

    Initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+
+const socketio = require('socket.io');
+const http = require('http');
+
+const routes = require("./routes");
+
+const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+
+mongoose.connect(process.env.mongodbCredential, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+    const { user_id } = socket.handshake.query;
+
+    connectedUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+    
+    return next();
+});
+app.use(cors());
+app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
+app.use(routes);
+
+server.listen(3333);
